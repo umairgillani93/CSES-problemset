860d1b2f81646d6f130c7cf1033b541edc3574ad
umairgillani93 <umairgillani93@gmail.com>
2023-01-07 00:08:25 +0500

    code added

--- spiral.cpp
+++ spiral.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
+	int t;
+	cin >> t;
+	vector<vector<int>> v{{1,2,9,10,25}, {4,3,8,11,24}, {5,6,7,12,23},{16,15,14,13,22},{17,18,19,20,21}};
 
+	for (int i = 0; i < t; i++) {
+		int y, x;
+		cin >> y >> x;
+		cout << v[y][x] << '\n';
+ }
+
+	return 0;
 }
