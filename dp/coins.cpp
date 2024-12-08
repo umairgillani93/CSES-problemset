af96bd31a62ab261c4835aabb035209ec5d6d947
umairgillani93 <umairgillani93@gmail.com>
2024-12-08 22:44:01 +0500

    coins problem dp

--- dp/coins.cpp
+++ dp/coins.cpp
@@ -2,32 +2,37 @@
 
 using namespace std;
 
-int dp(vector<int> &arr, int k) {
-	if (k == 0) return 0; // no more elements needed to form the sum 
-	if (k < 0) return INT_MAX; // invalid combination
-	
+int dp(vector<int> &arr, int k, vector<bool> &seen, vector<int> &cache) {
+	if (k == 0) return 0; // no more countings
+	if (k < 0)  return INT_MAX; // in-valid combination
+	if (seen[k]) return cache[k];
+
 	int ans = INT_MAX;
-	int n = arr.size();
-	for (int i = 0; i < n; i++) {
-		int res = dp(arr, k - arr[i]);
-		if (res != INT_MAX) {
+	for (int i = 0; i < arr.size(); i++) {
+		int res = dp(arr, k - arr[i], seen, cache);
+		if (res  != INT_MAX) {
 			ans = min(ans, 1 + res);
 		}
-
 	}
+	cache[k] = ans;
+	seen[k] = true;
 	return ans;
-
 }
 
 int main() {
 	int n, k;
 	cin >> n >> k;
-	vector<int> arr(n);
+	vector<int> a(n);
+	vector<bool> seen(k + 1, false);
+	vector<int> cache(k + 1, -1);
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		cin >> a[i];
 	}
-	long long res = dp(arr, k);
-	cout << res << '\n';
+	
+	int res = dp(a, k, seen, cache);
+	cout << ((res < INT_MAX) ? res : -1) << '\n';
 	return 0;
 
 }
+
+
